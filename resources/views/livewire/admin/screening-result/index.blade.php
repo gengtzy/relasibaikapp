457ce65849901e3cae78ef90c054c41ff73a26c0
gengtzy <agengpujipangestu3@gmail.com>
2025-12-14 18:18:36 +0700

    tes

--- resources/views/livewire/admin/screening-result/index.blade.php
+++ resources/views/livewire/admin/screening-result/index.blade.php
@@ -140,7 +140,6 @@
                                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
                                 </div>
                             </td>
-
                             {{-- ID Sesi Custom --}}
                             <td wire:click="viewResult({{ $item->id }})"
                                 class="px-6 py-4 font-medium whitespace-nowrap cursor-pointer">
